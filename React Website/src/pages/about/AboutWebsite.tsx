efcfba39bdad51936063970d9e6f6a6ccf80601c
MattX101 <matthew.xuereb@protonmail.com>
2023-09-28 14:14:38 +0200

    Uploaded git project to GitHub & Added GitHub link in About Website page

--- React Website/src/pages/about/AboutWebsite.tsx
+++ React Website/src/pages/about/AboutWebsite.tsx
@@ -3,6 +3,7 @@ import '../../styles/SplitTab.css'
 import React from 'react';
 import MainPagesNavMenu from '../../components/MainPagesNavMenu.tsx'
 import reactLogo from '../../images/React Logo.png';
+import dreamHostLogo from '../../images/DreamHost Logo.png';
 
 function LoadPage() {
     return (
@@ -15,13 +16,32 @@ function LoadPage() {
                 <br />
 
                 <p>Powered by the <b>React</b> Framework</p>
-                <img src={reactLogo} className="reactLogo" alt="logo" />
+                <img src={reactLogo} className="logoBig" alt="logo" />
                 <br /> <br />
 
                 <div id="split">
                     <p id="splitBy2"><b>TypeScript</b> utilsed</p>
                     <p id="splitBy2">Styled by <b>LESS</b></p>
                 </div>
+                <br />
+
+                <br />
+                <hr/>
+                <br />
+
+                <p><b>Hosted</b> on DreamHost</p>
+                <img src={dreamHostLogo} className="logoSmall" alt="logo" />
+                <br/><br/>
+
+                <p>Why mathew-xuereb.com?</p>
+                <p>Becuase matthewxuereb.com was taken.</p>
+                <br />
+
+                <hr/>
+                <br />
+
+                <h3>GitHub Link:</h3>
+                <p>https://github.com/MattX101/Portfolio-Website</p>
                 <br />
             </div>
 
